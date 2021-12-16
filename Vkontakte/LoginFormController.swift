1b4c65ccbb7e8933ee4dad5e4bc40a12a2b1d569
Олег Борель <olegborel@gmail.com>
2021-12-16 20:52:59 +0300

    Revert "lesson 2"
    This reverts commit e131d5a7e1f7feaeb1393717c5223a9e851c7ff9.

--- Vkontakte/LoginFormController.swift
+++ Vkontakte/LoginFormController.swift
@@ -6,69 +6,14 @@
 //
 
 import UIKit
-import WebKit
 
-
-class LoginFormController: UIViewController, WKNavigationDelegate {
-    @IBOutlet weak var webView: WKWebView!{
-        didSet {
-                    webView.navigationDelegate = self
-                }
-    }
+class LoginFormController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        authorizeToVK()
     }
-    func authorizeToVK() {
-            var urlComponents = URLComponents()
-            urlComponents.scheme = "https"
-            urlComponents.host = "oauth.vk.com"
-            urlComponents.path = "/authorize"
-            urlComponents.queryItems = [
-                URLQueryItem(name: "client_id", value: "8028799"),
-                URLQueryItem(name: "display", value: "mobile"),
-                URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
-                URLQueryItem(name: "scope", value: "262150"),
-                URLQueryItem(name: "response_type", value: "token"),
-                URLQueryItem(name: "revoke", value: "1"),
-                URLQueryItem(name: "v", value: "5.68")
-            ]
-
-            let request = URLRequest(url: urlComponents.url!)
-            webView.load(request)
-        }
-    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
-
-            guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
-
-                print(navigationResponse.response.url)
-                decisionHandler(.allow)
-                return
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func buttonLogin(_ sender: Any) {
             }
-    let params = fragment
-                        .components(separatedBy: "&")
-                        .map { $0.components(separatedBy: "=") }
-                        .reduce([String: String]()) { result, param in
-                                        var dict = result //буфер
-                                        let key = param[0]
-                                        let value = param[1]
-                                        dict[key] = value
-                                        return dict
-                                }
-
-            guard let token = params["access_token"], let userId = params["user_id"] else { return }
-
-            Session.shared.token = token
-            Session.shared.userId = userId
-
-            performSegue(withIdentifier: "showFriendsSegue", sender: nil)
-
-            print(url)
-
-            decisionHandler(.cancel)
-
-        }
 }
-
-
 
